bdf5997f7e0b71bc7651acfb92aaead94cb01b18
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-21 20:57:22 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const items = [
+    { name: "bee", image: "bee.png" },
+    { name: "crocodile", image: "crocodile.png" },
+    { name: "macaw", image: "macaw.png" },
+    { name: "gorilla", image: "gorilla.png" },
+    { name: "tiger", image: "tiger.png" },
+    { name: "monkey", image: "monkey.png" },
+    { name: "chameleon", image: "chameleon.png" },
+    { name: "piranha", image: "piranha.png" },
+    { name: "anaconda", image: "anaconda.png" },
+    { name: "sloth", image: "sloth.png" },
+    { name: "cockatoo", image: "cockatoo.png" },
+    { name: "toucan", image: "toucan.png" },
+  ];
+  
+  const generateRandom = (size = 4) => {
+    let tempArray = [...items];
+    let cardValues = [];
+    size = (size * size) / 2;
+    for (let i = 0; i < size; i++) {
+      const randomIndex = Math.floor(Math.random() * tempArray.length);
+      cardValues.push(tempArray[randomIndex]);
+      tempArray.splice(randomIndex, 1);
+    }
+    return cardValues;
+  };
+  
+  const matrixGenerator = (cardValues, size = 4) => {
+    const gameContainer = document.querySelector(".game-container");
+    gameContainer.innerHTML = "";
+    cardValues = [...cardValues, ...cardValues];
+    cardValues.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < size * size; i++) {
+      gameContainer.innerHTML += `
+        <div class="card-container" data-card-value="${cardValues[i].name}">
+          <div class="card-before">?</div>
+          <div class="card-after">
+            <img src="${cardValues[i].image}" class="image" />
+          </div>
+        </div>
+      `;
+    }
+    gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+  };
+  
+  const cardValues = generateRandom();
+  matrixGenerator(cardValues);
+  
